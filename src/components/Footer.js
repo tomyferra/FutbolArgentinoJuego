d7e2c5f066c64e999c46d84fd06d5e93d96e4724
tomyferra <ferra.tomy@gmail.com>
2024-04-03 12:00:04 -0300

    Contact

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,11 +8,13 @@ function Footer() {
     <div id='Contact' className=' max-w-[1240px] mx-auto p-4 text-gray border-gray-600 m-10'>
       <div className='flex justify-center items-center'>
         <a className=" text-white" href='mailto:ferra.tomy@gmail.com' rel='noopener' target='_blank'>
-          <IoMailOpenOutline className="text-white flex justify-center object-center items-center my-auto" size={30}/>
+          <div className='flex p-2'>
+            <IoMailOpenOutline className="text-white flex justify-center object-center items-center my-auto" size={30}/>
+            <p className='p-2'>Contactame!</p>
+          </div>
         </a>
       </div>
     </div>
-
   )
 }
 
